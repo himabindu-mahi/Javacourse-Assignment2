44f0cceeb277a0c5dc335d277098ece0e3882ce1
himabindu-mahi <himabindu@sellerware.com>
2016-03-03 16:45:05 +0530

    Added files via upload

---
+++ VariableAssignment2.java
@@ -0,0 +1,37 @@
+class VariableAssignment2
+{
+	public static void main(String args[])
+	{
+	     int a,b=4,c;
+		 System.out.println("B value " +b);
+		 c=10;
+		 a=20;
+		 int d=a+c;
+		 int e=d*a;
+		 int f=e-d;
+		 int g=e/a;
+		 System.out.println("Sum of two numbers:  " +d);
+		 System.out.println("Subtraction of two numbers:  " +f);
+		 System.out.println("Multiplication of two numbers:  " +e);
+		 System.out.println("Division of two numbers:   " +(50/10));
+		 System.out.println("AND operation for two variables:  "+ (a&b));
+		 System.out.println("OR operation for two variables:  " +(a|b));
+		 
+		 
+	}
+} 
+
+
+/* 
+
+Output:--
+
+B value 4
+Sum of two numbers:  30
+Subtraction of two numbers:  570
+Multiplication of two numbers:  600
+Division of two numbers:  5
+AND operation for two variables:  4
+OR operation for two variables:  20
+
+*/
